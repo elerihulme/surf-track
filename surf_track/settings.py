7b21ea7c966cec58967b23258c6373d382daef6f
Eleri Hulme <eleri.hulme@live.co.uk>
2024-12-17 18:25:06 +0000

    add secret key

--- surf_track/settings.py
+++ surf_track/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&pz)w7y)s#l@my3n&cs)!^zcxope+-xdx21!yu8xc5f6euy_8j'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
