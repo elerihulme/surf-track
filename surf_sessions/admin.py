0eda7d7788d4313652c307c9a9f5a25d502333d9
Eleri Hulme <eleri.hulme@live.co.uk>
2024-12-22 13:00:48 +0000

    add summernote to admin panel

--- surf_sessions/admin.py
+++ surf_sessions/admin.py
@@ -1,5 +1,15 @@
 from django.contrib import admin
 from .models import Session
+from django_summernote.admin import SummernoteModelAdmin
+
+
+@admin.register(Session)
+class SessionAdmin(SummernoteModelAdmin):
+
+    list_display = ('user', 'date', 'get_location_display')
+    search_fields = ['user__username']
+    list_filter = ('date', 'location')
+    summernote_fields = ('notes',)
 
 # Register your models here.
-admin.site.register(Session)
+
